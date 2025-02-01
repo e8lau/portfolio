e625a99763313d1414b0e2a7346e9e3fafc4ee0e
Ethan-Lau <42027797+e8lau@users.noreply.github.com>
2025-01-31 22:13:47 -0800

    Create global.js

---
+++ global.js
@@ -0,0 +1,36 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+    color: #333;
+}
+.container {
+    width: 80%;
+    margin: auto;
+    overflow: hidden;
+    padding: 20px;
+}
+.header {
+    text-align: center;
+    padding: 50px;
+    background: #333;
+    color: #fff;
+}
+.header img {
+    width: 150px;
+    border-radius: 50%;
+}
+.section {
+    margin: 20px 0;
+    padding: 20px;
+    background: #fff;
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+}
+ul {
+    list-style: none;
+    padding: 0;
+}
+ul li {
+    margin: 10px 0;
+}
